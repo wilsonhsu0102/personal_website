90028ceb0388d5e8dcf825a3bcf2055f134aa304
Wilson Hsu <wilsonhsu0102@gmail.com>
2021-02-05 22:46:29 -0500

    added resume button, style contact, added copyright footer

--- my-app/src/Components/AboutMe.jsx
+++ my-app/src/Components/AboutMe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Styles/AboutMe.css'
-import { Image, Row, Col } from 'react-bootstrap'
+import { Button, Image, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faFacebookF, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
 import Particles from 'react-particles-js';
@@ -64,7 +64,7 @@ class AboutMe extends React.Component {
                 </div>
 
                 <Row className="about-info-box justify-content-center">
-                    <Col xs="12" md="3">
+                    <Col xs={{ span: 12 }} md={{ span: 3 }}>
                         <Row><Image className="about-profile-pic" src={profilePic} roundedCircle /></Row>
                         <Row>
                             <Col className="about-icon about-icon-first-row" xs="3" md="6">
@@ -90,17 +90,26 @@ class AboutMe extends React.Component {
 
                         </Row>
                     </Col>
-                    <Col className="about-descriptions" xs="12" md={{ offset: 1, span: 8 }}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non eros velit.
-                        Aenean id porttitor turpis. Integer sed mi pretium, sagittis augue in, commodo odio.
-                        Vestibulum cursus sollicitudin sem, eu sollicitudin felis consequat nec. Etiam eleifend
-                        feugiat ex sed ornare. Nam a sapien rhoncus, dignissim tellus non, interdum nisi. Donec
-                        sit amet nisi aliquet, lobortis dui sed, egestas mauris. Pellentesque luctus viverra
-                        ligula sit amet facilisis. Integer eu magna sit amet quam suscipit gravida. Etiam in
-                        viverra dolor, ut porta magna. Ut ut urna eget quam maximus egestas. Mauris a ante in
-                        neque interdum fermentum nec non velit. Sed accumsan nisl quis quam imperdiet imperdiet.
-                        Etiam in viverra dolor, ut porta magna. Ut ut urna eget quam maximus egestas.
+                    <Col xs={{ span: 12 }} md={{ offset: 1, span: 8 }}>
+                        <Row>
+                            <Col className="about-descriptions" xs={{ span: 12, order: 2}} md={{ span: 12, order: 1 }}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non eros velit.
+                                Aenean id porttitor turpis. Integer sed mi pretium, sagittis augue in, commodo odio.
+                                Vestibulum cursus sollicitudin sem, eu sollicitudin felis consequat nec. Etiam eleifend
+                                feugiat ex sed ornare. Nam a sapien rhoncus, dignissim tellus non, interdum nisi. Donec
+                                sit amet nisi aliquet, lobortis dui sed, egestas mauris. Pellentesque luctus viverra
+                                ligula sit amet facilisis. Integer eu magna sit amet quam suscipit gravida. Etiam in
+                                viverra dolor, ut porta magna. Ut ut urna eget quam maximus egestas. Mauris a ante in
+                                neque interdum fermentum nec non velit. Sed accumsan nisl quis quam imperdiet imperdiet.
+                                Etiam in viverra dolor, ut porta magna. Ut ut urna eget quam maximus egestas.
+                            </Col>
+                            <Col xs={{ span: 12, order: 1 }} md={{ span: 12, order: 2 }} className="about-resume justify-content-center">
+                                <Button className="about-resume-btn"> Download Resume </Button>
+                            </Col>
+                        </Row>
+
                     </Col>
+
                 </Row>
             </div>
         );
--- my-app/src/Components/ContactMe.jsx
+++ my-app/src/Components/ContactMe.jsx
@@ -1,10 +1,14 @@
 import Particles from 'react-particles-js';
 import React from 'react';
 import '../Styles/ContactMe.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebookF, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 
 class ContactMe extends React.Component {
     render() {
+        let currentYear = new Date().getFullYear();
         return (
             <div id="contact">
                 <div className="contact-particle">
@@ -14,51 +18,118 @@ class ContactMe extends React.Component {
                         params={{
                             "particles": {
                                 "number": {
-                                    "value": 150,
+                                    "value": 200,
                                     "density": {
                                         "enable": true,
-                                        "value_area": 1000
+                                        "value_area": 800
                                     }
                                 },
-                                "color": {
-                                    "value": ["#d2ffff", "#d2d2ff", "#ffd2d2", "#ffffd2"]
-                                },
-                                "size": {
-                                    "value": 5
-                                },
                                 "line_linked": {
-                                    "enable": true,
-                                    "distance": 150,
-                                    "opacity": 0.5
+                                    "enable": false,
+                                    "opacity": 0.02
                                 },
                                 "move": {
-                                    "speed": 5
+                                    "direction": "random",
+                                    "speed": 0.5
+                                },
+                                "size": {
+                                    "value": 1
                                 },
                                 "opacity": {
                                     "anim": {
                                         "enable": true,
-                                        "opacity_min": 0.1,
-                                        "speed": 1,
-                                        "sync": false
+                                        "speed": 1.2,
+                                        "opacity_min": 0.05
                                     }
                                 }
                             },
                             "interactivity": {
                                 "events": {
-                                    "onhover": {
+                                    "onclick": {
                                         "enable": true,
-                                        "mode": "repulse"
+                                        "mode": "push"
+                                    }
+                                },
+                                "modes": {
+                                    "push": {
+                                        "particles_nb": 1
                                     }
                                 }
-                            }
+                            },
+                            "retina_detect": true
                         }} />
                 </div>
 
                 <Container className="contact-container">
                     <Row>
-                        <Col xs="12" md="5"> You can contact me at: wilsonhsu0102@gmail.com </Col>
-                        <Col xs="12" md="2"> <div className="contact-horizontal-line"> </div> <div className="contact-or">or</div> <div className="contact-horizontal-line"> </div></Col>
-                        <Col xs="12" md="5"> Leave me a message </Col>
+                        <Col xs="12" md="5">
+                            <Row className="justify-content-center">You can contact me at:</Row>
+                            <Row className="contact-form-row justify-content-center">wilsonhsu0102@gmail.com</Row>
+                            <Row className="contact-form-row justify-content-center">
+                                <Col className="contact-icon" xs="3">
+                                    <a href="#" className="about-icon-effect">
+                                        <FontAwesomeIcon icon={faInstagram} size="2x" />
+                                    </a>
+                                </Col>
+                                <Col className="contact-icon" xs="3">
+                                    <a href="#" className="about-icon-effect">
+                                        <FontAwesomeIcon icon={faGithub} size="2x" />
+                                    </a>
+                                </Col>
+                                <Col className="contact-icon" xs="3">
+                                    <a href="#" className="about-icon-effect">
+                                        <FontAwesomeIcon icon={faFacebookF} size="2x" />
+                                    </a>
+                                </Col>
+                                <Col className="contact-icon" xs="3">
+                                    <a href="#" className="about-icon-effect">
+                                        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+                                    </a>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col xs="12" md="1">
+                            <div className="contact-horizontal-line"> </div> <div className="contact-or">or</div> <div className="contact-horizontal-line"> </div>
+                        </Col>
+                        <Col xs="12" md="6">
+                            <Row className="justify-content-center">Leave me a message</Row>
+                            <Form.Row className="contact-form-row" controlId="formSenderName">
+                                <Col xs="3">
+                                    <Form.Label className="contact-msg-category">Your name:</Form.Label>
+                                </Col>
+                                <Col xs="9">
+                                    <Form.Control type="name" placeholder="John Doe" />
+                                </Col>
+                            </Form.Row>
+                            <Form.Row className="contact-form-row" controlId="formSenderInfo">
+                                <Col xs="3">
+                                    <Form.Label className="contact-msg-category">Contact method:</Form.Label>
+                                </Col>
+                                <Col xs="9">
+                                    <Form.Control type="contactMethod" placeholder="john.doe@email.com" />
+                                </Col>
+                            </Form.Row>
+                            <Form.Row className="contact-form-row" controlId="formInfo">
+                                <Col xs="3">
+                                    <Form.Label className="contact-msg-category">Let me know:</Form.Label>
+                                </Col>
+                                <Col xs="9">
+                                    <Form.Control className="contact-msg-textArea" as="textarea" rows={3} type="info" />
+                                </Col>
+                            </Form.Row>
+                            <Row className="contact-form-row contact-msg-send">
+                                <Button className="contact-send-btn" variant="primary" type="submit">
+                                    Send
+                            </Button>
+                            </Row>
+
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-center copy-right-row1">
+                        <small> &copy; Copyright 2020 - {currentYear} </small>
+                    </Row>
+                    <Row className="justify-content-center copy-right-row2">
+                        <small> All Rights Reserved. </small>
                     </Row>
                 </Container>
             </div >
