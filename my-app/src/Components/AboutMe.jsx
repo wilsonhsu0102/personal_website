14666f059ebd0b984d3ec4982318c5db7df855f4
Wilson Hsu <wilsonhsu0102@gmail.com>
2021-03-04 23:47:32 -0500

    added horizontal title bar into about page

--- my-app/src/Components/AboutMe.jsx
+++ my-app/src/Components/AboutMe.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../Styles/AboutMe.css';
 import ParticlesConfig from '../Resources/about-particle-config.json';
-import { Button, Image, Row, Col } from 'react-bootstrap';
+import { Button, Image, Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import Particles from 'react-particles-js';
@@ -18,7 +18,7 @@ class AboutMe extends React.Component {
 
     changeProfile = () => {
         this.setState({
-            profileIdx: this.state.profileIdx === this.content.profileImgs.length - 1 ? 0 : this.state.profileIdx + 1 
+            profileIdx: this.state.profileIdx === this.content.profileImgs.length - 1 ? 0 : this.state.profileIdx + 1
         })
     }
 
@@ -34,25 +34,33 @@ class AboutMe extends React.Component {
         })
         let profileImg = this.content.profileImgs[this.state.profileIdx]
         return (
-            <div id={this.content.id}>
+            <div id={this.content.id} className="page-div">
                 <div className="particle-div">
                     <Particles
                         className="particle-bg"
                         canvasClassName="particle-behind"
                         params={ParticlesConfig} />
                 </div>
-                <div className="page-div">
-                    <Row className="about-info-box justify-content-center">
-                        <Col className="about-title-row" xs={{ span: 12 }}><div className="section-title about-title">{this.content.title}</div></Col>
+                <Container className="about-info-box">
+                    <Row className="justify-content-center">
+                        <Col xs={{ offset: 0 }} md={{ offset: 1 }}> <div className="title-horizontal-left"> </div> </Col>
+                        <Col xs="auto" className="section-title about-title">{this.content.navText}</Col>
+                        <Col> <div className="title-horizontal-right"> </div> </Col>
+                        <Col xs="0" md="1"></Col>
+                        {/* <Col className="about-title-row" xs={{ span: 12 }}><div className="section-title about-title">{this.content.title}</div></Col> */}
+
+                    </Row>
+                    <Row className="experience-content justify-content-center" noGutters>
+                        {/* <Col className="about-title-row" xs={{ span: 12 }}><div className="section-title about-title">{this.content.title}</div></Col> */}
                         <Col xs={{ span: 12 }} md={{ span: 3 }}>
-                            <Row><Image className="about-profile-pic" src={profileImg} roundedCircle onClick={this.changeProfile}/></Row>
+                            <Row noGutters><Image className="about-profile-pic" src={profileImg} roundedCircle onClick={this.changeProfile} /></Row>
                             <Row>
                                 {contactLinks}
                             </Row>
                         </Col>
                         <Col xs={{ span: 12 }} md={{ offset: 1, span: 8 }}>
                             <Row>
-                                <Col className="about-descriptions description" xs={{ span: 12, order: 2 }} md={{ span: 12, order: 1 }} dangerouslySetInnerHTML= { {__html: this.content.description} }>
+                                <Col className="about-descriptions description" xs={{ span: 12, order: 2 }} md={{ span: 12, order: 1 }} dangerouslySetInnerHTML={{ __html: this.content.description }}>
                                 </Col>
                                 <Col xs={{ span: 12, order: 1 }} md={{ span: 12, order: 2 }} className="about-resume justify-content-center">
                                     <Button className="about-resume-btn description" href={this.content.resume} download>
@@ -63,7 +71,13 @@ class AboutMe extends React.Component {
                             </Row>
                         </Col>
                     </Row>
-                </div>
+                    <Row className="justify-content-center end-row">
+                        <Col xs={{ offset: 0 }} md={{ offset: 1 }}>
+                            <div className="end-horizontal"> </div>
+                        </Col>
+                        <Col xs="0" md="1"></Col>
+                    </Row>
+                </Container>
 
             </div>
         );
